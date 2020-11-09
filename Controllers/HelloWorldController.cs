1638e632020f7cf70a91e81702f0e8c79774d7bd
Aleksandre <sandrokhosht@gmail.com>
2020-11-10 00:51:05 +0400

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            Movie movie = new Movie();
+            movie.Title = "dasa";
+            ViewData["Message"] = "Hello " + movie.Title;
+            ViewData["NumTimes"] = numTimes;
+           
+            int a= 12;
+            return View(a);
+        }
+    }
+}
---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+
+        //Remove Comment if error
+        //public string SearchString { get; set; }
+    }
+}
